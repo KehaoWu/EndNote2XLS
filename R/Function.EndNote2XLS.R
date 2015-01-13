1d6c68319f4d3e702372cb0f3562b430c2274942
KehaoWu <kehao.wu@gmail.com>
2015-01-13 09:27:09 -0500

    Version 1.1

--- R/Function.EndNote2XLS.R
+++ R/Function.EndNote2XLS.R
@@ -1,14 +1,55 @@
-Endnote2XLS = function(files=NULL,merge=F)
+trans = function(content)
+{
+  CBM = FALSE
+  if(grepl(pattern = "^[0-9]",perl = T,x = content[1]))
+    CBM = TRUE
+  
+  if(CBM)
+  {
+    content = content[grepl(pattern = "^[0-9]|:",perl = T,x = content)]
+    Start = (1:length(content))[grepl(pattern = "^[0-9]",perl = T,x = content)]
+    Labels = unlist(lapply(X = content[-Start],FUN = function(line)unlist(strsplit(x = line,split = ":"))[1]))
+  }else{
+    Start = (1:length(content))[grepl(pattern = "%0",x = content)]
+    Labels = unlist(lapply(X = content,FUN = function(line)unlist(strsplit(x = line,split = "\\s"))[1]))
+  }
+  
+  Stop = c(Start[-1]-1,length(content))
+  
+  
+  Labels = Labels[!is.na(Labels)]
+  Labels = unique(Labels)
+  
+  Result = matrix(data = "",nrow = length(Start),ncol = length(Labels))
+  colnames(Result) = Labels
+  pb = txtProgressBar(min = 1,max = length(Start),style = 3)
+  for(index in 1:length(Start))
+  {
+    subContent = content[Start[index]:Stop[index]]
+    for( line in subContent)
+    {
+      if(line == "")
+        next
+      if(CBM)
+        line_split = unlist(strsplit(x = line,split = ":"))
+      else
+        line_split = unlist(strsplit(x = line,split = "\\s"))
+      first = line_split[1]
+      second = paste(line_split[-1],collapse = " ")
+      cIndex = match(x = first,table = Labels)
+      Result[index,cIndex] = gsub(pattern = "^\\s+",replacement = "",x = paste(Result[index,cIndex],second),perl = T)
+    }
+    setTxtProgressBar(pb,index)
+  }
+  return(Result)
+}
+
+Endnote2XLSSingle = function(files=NULL)
 {
   if(is.null(files))
   {
     stop("Error: Please specify your file names.\n")
   }
-  outfileMerge = paste(dirname(files[1]),"Endnote2XLS.txt",sep="/")
-  outfileMergeCBM = paste(dirname(files[1]),"Endnote2XLS.CBM.txt",sep="/")
-  first = TRUE
-  firstCBM = TRUE
-  print(outfileMerge)
   for(file in files)
   {
     cat("Process file",file,"...\n")
@@ -20,70 +61,75 @@ Endnote2XLS = function(files=NULL,merge=F)
       next
     }
     content = readLines(con = file,encoding = "UTF-8")
-    CBM = FALSE
-    if(grepl(pattern = "^[0-9]",perl = T,x = content[1]))
-      CBM = TRUE
-    
-    if(CBM)
-    {
-      content = content[grepl(pattern = "^[0-9]|:",perl = T,x = content)]
-      Start = (1:length(content))[grepl(pattern = "^[0-9]",perl = T,x = content)]
-      Labels = unlist(lapply(X = content[-Start],FUN = function(line)unlist(strsplit(x = line,split = ":"))[1]))
-    }else{
-      Start = (1:length(content))[grepl(pattern = "%0",x = content)]
-      Labels = unlist(lapply(X = content,FUN = function(line)unlist(strsplit(x = line,split = "\\s"))[1]))
-    }
-      
-    Stop = c(Start[-1]-1,length(content))
-    
-    
-    Labels = Labels[!is.na(Labels)]
-    Labels = unique(Labels)
-    
-    Result = matrix(data = "",nrow = length(Start),ncol = length(Labels))
-    colnames(Result) = Labels
-    pb = txtProgressBar(min = 1,max = length(Start),style = 3)
-    for(index in 1:length(Start))
-    {
-      subContent = content[Start[index]:Stop[index]]
-      for( line in subContent)
-      {
-        if(line == "")
-          next
-        if(CBM)
-          line_split = unlist(strsplit(x = line,split = ":"))
-        else
-          line_split = unlist(strsplit(x = line,split = "\\s"))
-        first = line_split[1]
-        second = paste(line_split[-1],collapse = " ")
-        cIndex = match(x = first,table = Labels)
-        Result[index,cIndex] = gsub(pattern = "^\\s+",replacement = "",x = paste(Result[index,cIndex],second),perl = T)
-      }
-      setTxtProgressBar(pb,index)
-    }
-    if(!merge)
-      write.table(x = Result,file = outfile,quote = F,sep = "\t",col.names = T,row.names = F,fileEncoding = "UTF-8")
-    else
-    {
-      if(CBM)
-      {
-        if(firstCBM)
-        {
-          write.table(x = Result,file = outfileMergeCBM,quote = F,sep = "\t",col.names = T,row.names = F,fileEncoding = "UTF-8")
-          firstCBM = FALSE
-        }else{
-          write.table(x = Result,file = outfileMergeCBM,quote = F,sep = "\t",col.names = F,row.names = F,fileEncoding = "UTF-8",append = T)
-        }
-      }else{
-        if(first)
-        {
-          write.table(x = Result,file = outfileMerge,quote = F,sep = "\t",col.names = T,row.names = F,fileEncoding = "UTF-8")
-          first = FALSE
-        }else{
-          write.table(x = Result,file = outfileMerge,quote = F,sep = "\t",col.names = F,row.names = F,fileEncoding = "UTF-8",append = T)
-        }      }
-    }
+    Result = trans(content)
+    write.table(x = Result,file = outfile,quote = F,sep = "\t",col.names = T,row.names = F,fileEncoding = "UTF-8")
     cat("\n")
   }
+}
+
+Endnote2XLSCombine = function(files=NULL)
+{
+  if(is.null(files))
+  {
+    stop("Error: Please specify your file names.\n")
+  }
+  firstLine = NULL
+  for(file in files)
+  {
+    if(!file.exists(file))
+    {
+      cat("File",file,"is not exist and skip it...\n")
+      next
+    }
+    firstLine = c(firstLine,readLines(file,1,encoding = "UTF-8"))
+  }
+  isCBM = unlist(lapply(firstLine,FUN = function(line){
+    grepl(pattern = "^[0-9]|:",perl = T,x = line)
+  }))
+  
+  ##non-CBM
+  if(any(!isCBM))
+  {
+    content = NULL
+    for(file in files[!isCBM])
+    {
+      cat("Process file",file,"...\n")
+      if(!file.exists(file))
+      {
+        cat("File",file,"is not exist and skip it...\n")
+        next
+      }
+      content = c(content,readLines(file,encoding = "UTF-8"))
+    }
+    Result = trans(content)
+    outfile = paste(dirname(files[!isCBM][1]),"Endnote2XLS.txt",sep="/")
+    write.table(x = Result,file = outfile,quote = F,sep = "\t",col.names = T,row.names = F,fileEncoding = "UTF-8")
+    cat("\n")
+  }
+  if(any(isCBM))
+  {
+    content = NULL
+    for(file in files[isCBM])
+    {
+      cat("Process file",file,"...\n")
+      if(!file.exists(file))
+      {
+        cat("File",file,"is not exist and skip it...\n")
+        next
+      }
+      content = c(content,readLines(file,encoding = "UTF-8"))
+    }
+    Result = trans(content)
+    outfile = paste(dirname(files[!isCBM][1]),"Endnote2XLS.CBM.txt",sep="/")
+    write.table(x = Result,file = outfile,quote = F,sep = "\t",col.names = T,row.names = F,fileEncoding = "UTF-8")
+    cat("\n")
+  }
+}
+Endnote2XLS = function(files=NULL,combine=F)
+{
+  if(combine)
+    Endnote2XLSCombine(files)
+  else
+    Endnote2XLSSingle(files)
 }
 
--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # Endnote2XLS
 #### 中文版-使用说明
+##### 新版本特性
+1. 自动识别是否是CBM文件，无需指定选项
+2. 可选择是否合并输出文件
+
+##### 如何使用
 1. 需先安装devtools(仅安装一次):  
 > install.packages("devtools")
 
@@ -9,36 +14,16 @@
 3. 加载R包
 > library(Endnote2XLS)
 
-4. 对于非CBM的Endnote格式文件转换方法：
-> files = c("wanfang.txt","vp.txt")<br>
-> EndNote2XLS(files)
+4. 如果不需要合并输出结果
+   files=dir()的作用是获取当前工作路径下的所有文件名，前提是该工作路径下只含有你需要转换的文件
+> files = dir() <br>
+> Endnote2XLS(files)
 
-5. 对于CBM的文本文件格式转换方法：
-> files = c("CBM.txt")<br>
-> EndNote2XLS(files,CBM=TRUE)
+5. 如果需要合并输出结果
+   files=dir()的作用是获取当前工作路径下的所有文件名，前提是该工作路径下只含有你需要转换的文件
+> files = dir() <br>
+> Endnote2XLS(files,combine=T)
 
 6. 所有来自CBM、CNKI等网站的文件导出不需要做任何其他的调整，可以直接用于格式转换
 
-7. 多个文件可以先通过Windows的DOS命令来合并，以下命令在Windows下完成
-> copy * new.txt
-
-8. 转换后文件导入Excel时不要选择用空格分隔
-
-#### English-Version</h5>
-1. install package: devtools(just once)
-> install.packages("devtools")
-
-2. install Endnote2XLS(just once))
->devtools::install_github("KehaoWu/Endnote2XLS")
-
-3. load R package
-> library(Endnote2XLS)
-
-4. For files from non-CBM
-> files = c("wanfang.txt","vp.txt")<br>
-> EndNote2XLS(files)
-
-5. For files from CBM
-> files = c("CBM.txt")<br>
-> EndNote2XLS(files,CBM=TRUE)
-
+7. 转换后文件导入Excel时不要选择用空格分隔
